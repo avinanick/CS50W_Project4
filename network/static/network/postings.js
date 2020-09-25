497ba39232a24c181b3b22f6df722c00f74f5189
Nick Avina <avinanick@gmail.com>
2020-09-25 16:55:32 -0700

    Created a javascript skeleton for listing postings on any page.

---
+++ network/static/network/postings.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Here we should determine what page we are on, and load accordingly
+    
+  });
+
+  function create_posting_element(posting_json) {
+
+  }
+
+  function get_num_post_pages(user_set) {
+
+  }
+
+  function get_page_posts(user_set, page_number) {
+      
+  }
