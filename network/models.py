d95aeee53ae8fb7d1bb405b65d95bcc284c63359
Nick Avina <avinanick@gmail.com>
2020-09-25 16:21:59 -0700

    First commit

---
+++ network/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    subscriptions = models.ManyToManyField("User", related_name="followers")
+
+class Posting(models.Model):
+    poster = models.ForeignKey("User", on_delete=models.CASCADE, related_name="postings")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        pass # TODO: serialize to json
+
+class Reaction(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="reactions")
+    post = models.ForeignKey("Posting", on_delete=models.CASCADE, related_name="reactions")
+    value = models.IntegerField()
