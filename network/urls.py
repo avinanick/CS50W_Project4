bafd4e78b687e06ae1e7663ba8c2b4f7e3009eb2
Nick Avina <avinanick@gmail.com>
2020-09-26 10:54:37 -0700

    Added the url path for the create post api.

--- network/urls.py
+++ network/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("create", views.create_post, name="create_post"),
     path("posts/<str:type>/<int:page_number>", views.load_posts, name="load_posts")
 ]
