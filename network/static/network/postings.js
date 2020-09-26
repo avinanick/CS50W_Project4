377d7b5a8f039dc3f70ce011e655d9a6e9231cff
Nick Avina <avinanick@gmail.com>
2020-09-26 10:55:42 -0700

    Started making the javascript function that will respond to a new post submition.

--- network/static/network/postings.js
+++ network/static/network/postings.js
@@ -4,12 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
     if (document.getElementById('all_posts')) {
         // set the listeners for the all posts section
         get_page_posts("all", 1);
+        document.getElementById('new_post_form').addEventListener('submit', create_new_post);
     }
     if(document.getElementById('subscription_posts')) {
         get_page_posts("subscription", 1);
     }
     
   });
+
+function create_new_post() {
+
+    let post_content = document.getElementById('new_post_content').value;
+    const csrftoken = getCookie('csrftoken');
+
+}
 
 function create_posting_element(posting_json) {
 
@@ -29,6 +37,22 @@ function create_posting_element(posting_json) {
     post_timestamp.innerHTML = posting_json["timestamp"];
     like_count.innerHTML = posting_json["likes_count"];
 
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 
 function get_num_post_pages(user_set) {
