cccc5e523d8495ac751e0a57309a7d78b153d77a
Nick Avina <avinanick@gmail.com>
2020-09-25 19:45:59 -0700

    Created a serialize function for the posting model to convert its data into json.

--- network/models.py
+++ network/models.py
@@ -10,8 +10,15 @@ class Posting(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def serialize(self):
-        pass # TODO: serialize to json
+    def serialize(self, user):
+        return {
+            "id": self.id,
+            "poster": self.poster.username,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %-d %Y, %-I:%M %p"),
+            "likes_count": self.likes.count(),
+            "from_user": self.poster == user
+        }
 
 class Like(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="favorites")
