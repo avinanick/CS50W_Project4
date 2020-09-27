58b79e1d8a97aef4c70353bc2bce43e141f6e2e3
Nick Avina <avinanick@gmail.com>
2020-09-27 13:05:23 -0700

    First pass at the toggle_like url and view.

--- network/urls.py
+++ network/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("create", views.create_post, name="create_post"),
     path("subscriptions", views.subscription_view, name="subscriptions"),
     path("update_post", views.update_post, name="update_poast"),
+    path("toggle_like", views.toggle_like, name="toggle_like"),
     path('user/follow', views.follow_user, name="follow_user"),
     path("user/<str:profile_name>", views.profile_view, name="profile"),
     path("posts/<str:type>/<int:page_number>", views.load_posts, name="load_posts")
