500beb9754a04b0a3bf89e381c09bc9f3aa7e75d
Nick Avina <avinanick@gmail.com>
2020-09-26 20:47:45 -0700

    Registering models with admin site.

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Posting, Like
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Posting)
+admin.site.register(Like)
